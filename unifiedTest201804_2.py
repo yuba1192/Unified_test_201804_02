aba0982f78bc7a72efa9dbe4d1819605f3326f39
yuba1192 <yuba1192@gmail.com>
2018-05-02 13:36:35 +0900

    2nd commit

--- unifiedTest201804_2.py
+++ unifiedTest201804_2.py
@@ -15,18 +15,17 @@ def getTitle(url):
 
     return title[0][1]
 
-def main():
-    check_url = []
+# クローリング処理
+def crawling(url, check_url):
 
-    # アクセスするURL
-    url = "https://no1s.biz/"
     # html取得
     resp = requests.get(url)
 
     # aタグを取得
-    address = re.findall('<a href=\"(https://no1s.biz/.*?)\".*>(.*)</a>', resp.text)
+    address = re.findall('<a.*href=\"(https://no1s.biz/.*?)\".*>(.*)</a>', resp.text)
 
     for i in range(len(address)):
+        # すでに登場済みの場合はパス
         if address[i][0] in check_url:
             continue
 
@@ -38,6 +37,18 @@ def main():
 
         # 表示させる
         print(urllib.parse.unquote(address[i][0])    + "\t" + urlTitle)
+
+        # 再度取得したURL内をクローリング
+        crawling(urllib.parse.unquote(address[i][0]), check_url)
+
+
+def main():
+    check_url = []
+    # アクセスするURL
+    url = "https://no1s.biz/"
+
+    # クローリング処理
+    crawling(url, check_url)
 
 if __name__ == "__main__":
     main()
